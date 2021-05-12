c768c47f13c1b4a2128dd1801903a16fece32dc2
Michael Reimann <michael@reimann.science>
2021-05-12 16:29:19 +0200

    Cell density analyzer extracts volume now. Added notebook outlining a potential equalization strategy
    For details, see https://bbpteam.epfl.ch/project/issues/browse/BBPP82-575

--- cell-densities/nrrdhlp/mtypes.py
+++ cell-densities/nrrdhlp/mtypes.py
@@ -1,9 +1,11 @@
 import re
 
+from os import path
+
 mtype_regex = r"L\d+_[A-Z:]{2,6}"
 
 
 def parse_mtype(fn):
     for match in re.findall(mtype_regex, fn):
         return match
-    return None
+    return path.splitext(fn)[0]
