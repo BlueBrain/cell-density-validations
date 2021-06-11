e5602536d3c7a0b038a2d4abd01399f8069110b6
Michael Reimann <michael.reimann@epfl.ch>
2021-06-11 16:50:36 +0200

    Using existing parser for mtype names from file names

--- cell-densities/bin/extract_depth_profiles.py
+++ cell-densities/bin/extract_depth_profiles.py
@@ -8,17 +8,17 @@ import json
 from nrrdhlp.masker import Masker
 from nrrdhlp.hierarchyhlp import *
 from nrrdhlp.find_atlas_files import *
+from nrrdhlp.mtypes import parse_mtype
 
 
-def make_region_profiles(masker, depths, regions, density_fn, d_bins):
+def make_region_profiles(depths, regions, region_masks, density_fn, d_bins):
     print("Loading and analyzing {0}".format(density_fn))
-    name = os.path.split(os.path.splitext(density_fn)[0])[1].replace("[cell_density]", "")
+    name = parse_mtype(density_fn)
     density = voxcell.VoxelData.load_nrrd(density_fn)
     res = []
     idx = []
-    for region in regions:
-        msk = masker.mask(region)
-        d_idx = numpy.digitize(depths.raw[:, :, :, 1][msk], d_bins) - 1
+    for region, msk in zip(regions, region_masks):
+        d_idx = numpy.digitize(depths.raw[msk], d_bins) - 1
         d = density.raw[msk]
         dens_smpls = [d[d_idx == i] for i in range(len(d_bins) - 1)]
         res.append(list(map(numpy.mean, dens_smpls)))
@@ -45,8 +45,9 @@ def configure():
         hier = json.load(fid)["msg"][0]
     regions = cfg.get("regions", list_cortex_regions(hier))
     annotation_fn = cfg.get("annotations", find_regions(root))
-    depth_fn = cfg.get("depths", os.path.join(root, "[PH]1.nrrd"))
+    depth_fn = cfg.get("depths", os.path.join(root, "[PH]y.nrrd"))
     depths = voxcell.VoxelData.load_nrrd(depth_fn)
+    #depths.raw = depths.raw[:, :, :, 1]
     mx_depths = cfg.get("max_depth", numpy.nanmax(depths.raw))
     nbins = cfg.get("nbins", 200)
 
@@ -61,11 +62,12 @@ def configure():
 
 def main():
     density_files, hier, masker, depths, regions, out_fn, mx_depths, nbins = configure()
+    region_masks = [masker.mask(region) for region in regions]
     d_bins = numpy.linspace(0, mx_depths + 1E-9, nbins + 1)
 
     res = []
     for fn in density_files:
-        res.append(make_region_profiles(masker, depths, regions, fn, d_bins))
+        res.append(make_region_profiles(depths, regions, region_masks, fn, d_bins))
 
     pandas.concat(res, axis=1).to_pickle(out_fn)
 
