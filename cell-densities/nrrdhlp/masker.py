0366ef9b35b6d5b17484f4bcabf79518f9954821
Michael Reimann <michael.reimann@epfl.ch>
2022-10-28 09:51:46 +0200

    Added instantiation using the atlas in the knowledge graph

--- cell-densities/nrrdhlp/masker.py
+++ cell-densities/nrrdhlp/masker.py
@@ -1,3 +1,4 @@
+from importlib.metadata import distribution
 import numpy
 
 from voxcell import VoxelData
@@ -7,19 +8,40 @@ from voxcell.nexus.voxelbrain import RegionMap
 layer_match_pat = "@.*{0}[ab]?$"
 layer_match_prop = "acronym"
 
+default_endpoint = "https://bbp.epfl.ch/nexus/v1"
+production_bbp_atlas = "https://bbp.epfl.ch/neurosciencegraph/data/4906ab85-694f-469d-962f-c0174e901885"
+
+
+def download_and_read(forge, thing, reader, distribution_id=None):
+    print("Downloading: {0}".format(thing))
+    if distribution_id is None: distribution_id = 0
+    if not isinstance(thing.distribution, list):
+        distribution = [thing.distribution]
+    else:
+        distribution = thing.distribution
+    forge.download(thing, "distribution.contentUrl", ".", overwrite=True)
+
+    return reader(distribution[distribution_id].name)
+
 
 class Masker(object):
     """
     A helper class that generates bitwise masks for brain volumes according to custom specifications
     """
 
-    def __init__(self, hier_fn, annotation_fn):
+    def __init__(self, hier, annotation):
         """
-        :param hier_fn: path to hierarchy.json file
-        :param annotation_fn: path to brain region annotation volume file
+        :param hier: path to hierarchy.json file, or a RegionMap
+        :param annotation: path to brain region annotation volume file, or the annotation volume
         """
-        self.rmap = RegionMap.load_json(hier_fn)
-        self.annotation = VoxelData.load_nrrd(annotation_fn)
+        if isinstance(hier, RegionMap):
+            self.rmap = hier
+        else:
+            self.rmap = RegionMap.load_json(hier)
+        if isinstance(annotation, VoxelData):
+            self.annotation = annotation
+        else:
+            self.annotation = VoxelData.load_nrrd(annotation)
 
     def mask(self, specs):
         """
@@ -27,7 +49,7 @@ class Masker(object):
         :return: bitwise mask according to specifications
         """
         lst_ids = self.target_ids(specs)
-        return numpy.in1d(self.annotation.raw.flat, lst_ids).reshape(self.annotation.raw.shape)
+        return numpy.isin(self.annotation.raw, lst_ids)
 
     def target_ids(self, specs):
         if isinstance(specs, str):
@@ -53,3 +75,29 @@ class Masker(object):
             return base_result
         else:
             raise ValueError("Mask specification error: {0}".format(str(specs)))
+    
+    @classmethod
+    def from_knowledge_graph(cls, yml_config, endpoint=None, token=None):
+        import getpass
+        from kgforge.core import KnowledgeGraphForge
+
+        if token is None:
+            token = getpass.getpass()
+        if endpoint is None:
+            endpoint = default_endpoint
+        print("Setting up kgforge...")
+        forge = KnowledgeGraphForge(yml_config,
+                            token=token,
+                            endpoint=endpoint, 
+                            bucket="bbp/atlas")
+        atlas_release = forge.retrieve(production_bbp_atlas)
+        atlas_release._store_metadata["_rev"]
+        parcellation_ontology = forge.retrieve(atlas_release.parcellationOntology.id, cross_bucket=True)
+        parcellation_volume = forge.retrieve(atlas_release.parcellationVolume.id)
+        print("...done!")
+        print("Starting downloads...")
+        ann = download_and_read(forge, parcellation_volume, VoxelData.load_nrrd)
+        hier = download_and_read(forge, parcellation_ontology, RegionMap.load_json)
+        print("...done!")
+        return cls(hier, ann)
+        
