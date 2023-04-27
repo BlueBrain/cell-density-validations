a75af180ff3ae1d0d5ec4c81cfdcf97b61f3f75d
Michael Reimann <michael.reimann@epfl.ch>
2023-04-27 10:53:52 +0200

    Added alternative method to fixing non-leaves
    The new method instead creates a new leaf under the existing region, using its name and acronym and appending a suffix.
    One can specify which method to use for which regions in an optional dict.

--- cell-densities/nrrdhlp/region_annotations.py
+++ cell-densities/nrrdhlp/region_annotations.py
@@ -6,7 +6,7 @@ from scipy.spatial import KDTree
 from .atlas_from_forge import hierarchy_from_forge, annotation_from_forge
 
 class AnnotationWrapper(object):
-    def __init__(self, forge):
+    def __init__(self, forge, method_dict={}):
         self._ann = annotation_from_forge(forge)
         self.hier = hierarchy_from_forge(forge)
         self.voxel_counts = pandas.Series(self._ann.raw.flatten()).value_counts()
@@ -21,6 +21,7 @@ class AnnotationWrapper(object):
             "z": Z.flatten().astype(float),
             "d": self._ann.raw.flatten()
         }).set_index("d")
+        self.methods = method_dict.copy()
 
     @staticmethod
     def __nearest_n_neighbor_extrapolator__(child_coords, reg_coords, n=6):
@@ -39,6 +40,39 @@ class AnnotationWrapper(object):
         evaluated = df.groupby(["lcl_idx", "neighbor_ids"])["inverse_distance"].sum().unstack("neighbor_ids")
         evaluated = evaluated.idxmax(axis="columns").sort_index()
         return evaluated
+    
+    def lookup_method(self, reg_id):
+        reg_acronym = self.hier.get(reg_id, "acronym")
+        if reg_acronym in self.methods:
+            return self.methods[reg_acronym]
+        parent = self.hier._parent[reg_id]
+        if parent is None:
+            return "create"
+        return self.lookup_method(parent)
+    
+    def make_new_region_id(self):
+        return int(numpy.max(list(self.hier._data.keys())) + 1 + numpy.random.randint(10))
+    
+    def make_new_region(self, parent_id, **kwargs):
+        parent_name = self.hier.get(parent_id, "name")
+        parent_acronym = self.hier.get(parent_id, "acronym")
+        reg_name = parent_name + ": Other"
+        reg_acronym = parent_acronym + "_O"
+        reg_id = self.make_new_region_id()
+
+        self.hier._parent[reg_id] = parent_id
+        self.hier._children[parent_id].append(reg_id)
+        self.hier._children[reg_id] = []
+
+        entry_dict = {
+            "id": reg_id,
+            "acronym": reg_acronym,
+            "name": reg_name,
+            "parent_structure_id": parent_id
+        }
+        entry_dict.update(kwargs)
+        self.hier._data[reg_id] = entry_dict
+        return reg_id
     
     @property
     def ann(self):
@@ -68,13 +102,21 @@ class AnnotationWrapper(object):
         self.voxel_counts = self.voxel_counts.add(evaluated.value_counts(), fill_value=0)
         self.voxel_counts[reg_id] = 0
     
-    def __recursive_check__(self, reg_id, **kwargs):
+    def create_new_leaf_region(self, reg_id):
+        new_reg_id = self.make_new_region(reg_id)
+        L = numpy.sum(self.ann_df.index == reg_id)
+        self.ann_df.index.values[self.ann_df.index == reg_id] = new_reg_id
+        self.voxel_counts[new_reg_id] = L
+        self.voxel_counts[reg_id] = 0
+        self._input_voxel_counts[new_reg_id] = 0
+    
+    def __recursive_check__(self, reg_id, method=None, **kwargs):
         count_direct = self.voxel_counts.get(reg_id, 0)
         child_ids = self.hier._children[reg_id]
         count_indirect = 0
         log = ""
         for _id in child_ids:
-            _count, _log = self.__recursive_check__(_id)
+            _count, _log = self.__recursive_check__(_id, method=method, **kwargs)
             count_indirect += _count
             log += _log
 
@@ -84,7 +126,13 @@ class AnnotationWrapper(object):
                        count_direct + count_indirect,
                        count_direct,
                        (100.0 * count_direct) / (count_direct + count_indirect))
-            self.extrapolate_to_leaf_regions(reg_id, **kwargs)
+            method = method or self.lookup_method(reg_id)
+            if method == "extrapolate":
+                self.extrapolate_to_leaf_regions(reg_id, **kwargs)
+            elif method == "create":
+                self.create_new_leaf_region(reg_id)
+            else:
+                raise ValueError("Unknow method: {0}".format(method))
         return count_direct + count_indirect, log
     
     def launch_extrapolation(self, **kwargs):
@@ -96,9 +144,12 @@ class AnnotationWrapper(object):
 
         return log
     
-    def fix(self, fn_ann_out, fn_log, **kwargs):
+    def fix(self, fn_ann_out, fn_hier_out, fn_log, **kwargs):
         log_str = self.launch_extrapolation(**kwargs)
         self.ann.save_nrrd(fn_ann_out)
+        
+        with open(fn_hier_out, "w") as fid:
+            fid.write("This is where the updated hierarchy would go, but I don't know how to write it")
 
         log_str += "Changes in leaf volumes:\n\n"
         voxel_count_changes = self.voxel_counts.divide(self._input_voxel_counts)
